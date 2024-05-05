7ac86f0a866185f560817c8142412d20891e5ffa
sunman54 <m.sunman@yandex.com>
2024-05-05 22:34:47 +0300

    minor bug fix

--- api/index.py
+++ api/index.py
@@ -2,9 +2,11 @@ from flask import Flask, request, jsonify
 from api.gemini_chat import gemini
 from flask_cors import CORS
 
-CORS(app)
 
 app = Flask(__name__)
+
+CORS(app)
+
 
 @app.route('/')
 def home():
